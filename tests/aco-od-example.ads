c6d9660508203783c9c465d6968cb6b0aeae0042
jonashaggstrom <jonas.haggstrom@gmail.com>
2021-08-23 13:28:16 +0200

    Add files via upload

--- tests/aco-od-example.ads
+++ tests/aco-od-example.ads
@@ -1,5 +1,5 @@
-with ACO.OD_Types.Entries;
 with ACO.Generic_Entry_Types;
+with ACO.OD_Types.Entries;
 
 package ACO.OD.Example is
    --  Shall be generated based on an EDS file
@@ -67,7 +67,7 @@ private
 
    --  0x1006 Communication Cycle Period VAR
 
-   Comm_Cycle_Per_Var : aliased Entry_U32 := Create (RW, 10_000);
+   Comm_Cycle_Per_Var : aliased Entry_Dur := Create (RW, 0.0000001);
 
    Comm_Cycle_Per_Data : aliased Entry_Array :=
       (0 => Comm_Cycle_Per_Var'Access);
--- tests/unit_tests.adb
+++ tests/unit_tests.adb
@@ -1,15 +1,27 @@
-with Generic_Table_Test;
 with Generic_Collection_Test;
+with Generic_Table_Test;
 with Remote_Node_Test;
 
 package body Unit_Tests is
 
    function Suite return Access_Test_Suite is
+
+      type T_GenericTableTest is access all Generic_Table_Test.Test'Class;
+      type T_GenericCollectionTest is access all Generic_Collection_Test.Test'Class;
+      type T_RemoteNodeTest is access all Remote_Node_Test.Test'Class;
+
       Ret : constant Access_Test_Suite := new Test_Suite;
+
+      GenericTableTest : constant T_GenericTableTest := new Generic_Table_Test.Test;
+
+      GenericCollectionTest : constant T_GenericCollectionTest := new Generic_Collection_Test.Test;
+
+      RemoteNodeTest : constant T_RemoteNodeTest := new Remote_Node_Test.Test;
+
    begin
-      Ret.Add_Test (new Generic_Table_Test.Test);
-      Ret.Add_Test (new Generic_Collection_Test.Test);
-      Ret.Add_Test (new Remote_Node_Test.Test);
+      Ret.Add_Test (GenericTableTest);
+      Ret.Add_Test (GenericCollectionTest);
+      Ret.Add_Test (RemoteNodeTest);
       return Ret;
    end Suite;
 
